7f795d2247a47066e17534a0126675c9fc93902e
amarchino <marchino.alessandro@gmail.com>
2026-01-06 12:03:38 +0100

    Lesson 5

--- test/test.js
+++ test/test.js
@@ -2,6 +2,21 @@ const assert = require('assert');
 
 describe('File to be tested', () => {
   context('Function to be tested', () => {
+
+    before(() => {
+      console.log('===before');
+    });
+    after(() => {
+      console.log('===after');
+    });
+
+    beforeEach(() => {
+      console.log('=====beforeEach');
+    })
+    afterEach(() => {
+      console.log('=====afterEach');
+    })
+
     it('should do something', () => {
       assert.equal(1, 1)
     });
